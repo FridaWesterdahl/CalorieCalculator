bf9c14194808310db25f0742284151de1d69e7f6
Frida <fridaawesterdahl@gmail.com>
2022-06-20 00:31:06 +0200

    Final

--- src/App.js
+++ src/App.js
@@ -6,13 +6,9 @@ let calories = 0;
 let loss = 0;
 let gain = 0;
 function App() {
-  const [calculated, setCalculated] = useState(false);
   const [age, setAge] = useState(0);
   const [height, setHeight] = useState(0);
   const [weight, setWeight] = useState(0);
-  const [activity, setActivity] = useState("");
-
-
 
   const bmrCount = () => {
     const male = document.querySelector("#male");
@@ -30,28 +26,32 @@ function App() {
 
   const calculate = (e) => {
     e.preventDefault();
-    setCalculated(!calculated);
     bmrCount(bmr);
-    calories = (bmr * 1.375).toFixed(0);
+
+    const activity = document.querySelector("#activity").value;
+    calories = (bmr * activity).toFixed(0);
 
     const diff = 200;
     loss = (parseInt(calories) - parseInt(diff));
     gain = (parseInt(calories) + parseInt(diff));
 
-    const maintenance = document.querySelector("#maintenance");
-    maintenance.textContent = `Maintenance calorie intake: ${calories}`;
     const weightloss = document.querySelector("#loss");
-    weightloss.textContent = `Weight loss calorie intake: ${loss}`;
+    weightloss.textContent = `Weight loss: ${loss} kcal`;
+    const maintenance = document.querySelector("#maintenance");
+    maintenance.textContent = `Maintenance calorie intake: ${calories} kcal`;
     const weightgain = document.querySelector("#gain");
-    weightgain.textContent = `Weight gain calorie intake: ${gain}`;
+    weightgain.textContent = `Weight gain: ${gain} kcal`;
+
+    const info = document.querySelector("#info");
+    info.classList.remove("hidden");
   }
 
   return (
     <div className="App">
-      <h1>Calorie calculator</h1>
+      <h1>Daily calorie calculator</h1>
 
       <div className='inputs'>
-        <form>
+        <form onSubmit={calculate}>
           <div className='gender'>
             <div className='male'>
               <input type="radio" id="male" name="gender" value="male"></input>
@@ -62,30 +62,47 @@ function App() {
               <label htmlFor="female">Female</label>
             </div>
           </div>
-          <input type="number" placeholder='Age (15-80)' min="15" max="80" required
-            onChange={(e) => setAge(e.target.value)}></input>
-          <input type="number" placeholder='Enter height in cm' min="150" required
-            onChange={(e) => setHeight(e.target.value)}></input>
-          <input type="number" placeholder='Enter weight in kg' required
-            onChange={(e) => setWeight(e.target.value)}></input>
+          <div className='wrapper'>
+            <input type="number" placeholder='Age (15-80)' min="15" max="80" required
+              onChange={(e) => setAge(e.target.value)}></input>
+            <p>years</p>
+          </div>
+          <div className='wrapper'>
+            <input type="number" placeholder='Enter height' min="150" max="200" required
+              onChange={(e) => setHeight(e.target.value)}></input>
+            <p>cm</p>
+          </div>
+          <div className='wrapper'>
+            <input type="number" placeholder='Enter weight' required
+              onChange={(e) => setWeight(e.target.value)}></input>
+            <p>kg</p>
+          </div>
 
-          <label htmlFor="activity">Activity level</label>
-          <select id="activity" required>
-            <option value="little">Little: little or no exercise</option>
-            <option value="light">Light: exercise 1-3 times/week</option>
-            <option value="moderate" selected>Moderate: exercise 4-5 times/week</option>
-            <option value="active">Active: daily exercise or intense exercise 3-4 times/week</option>
-            <option value="very-active">Very active: intense exercise 6-7 times/week</option>
-            <option value="extra-active">Extra active: intense exercise daily or physical job</option>
-          </select>
-          <button onClick={calculate}>Calculate</button>
+          <div className='activity'>
+            <label htmlFor="activity">Activity level</label>
+            <select id="activity" required>
+              <option value="1.2">Little: little or no exercise</option>
+              <option value="1.375">Light: exercise 1-3 times/week</option>
+              <option value="1.55" selected>Moderate: exercise 3-5 times/week</option>
+              <option value="1.55">Active: daily exercise or intense exercise 3-4 times/week</option>
+              <option value="1.725">Very active: intense exercise 6-7 times/week</option>
+              <option value="1.9">Extra active: intense exercise daily or physical job</option>
+            </select>
+          </div>
+          <button type="submit" onSubmit={calculate}>Calculate</button>
         </form>
       </div>
 
-      <div className={calculated ? 'calculations' : 'hidden'}>
-        <p id="maintenance">Maintenance calorie intake: {calories}</p>
-        <p id="loss">Weight loss calorie intake: {loss}</p>
-        <p id="gain">Weight gain calorie intake: {gain}</p>
+      <div className='calculations'>
+        <p id="loss"></p>
+        <p id="maintenance"></p>
+        <p id="gain"></p>
+      </div>
+
+      <div className='info'>
+        <i id="info" className='hidden'>These calculations are made by your basal metabolic rate (BMR),
+          the amount of energy that your body needs to function if it were to rest for 24 hours, and the
+          Harris-Benedict equation.</i>
       </div>
     </div>
   );
